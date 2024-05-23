93f773b81d6ccdc54d1103ec79c2264df9d8f0a8
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-05-23 20:16:38 +0300

    HealthBar update

--- Assets/Scripts/Characters/Health.cs
+++ Assets/Scripts/Characters/Health.cs
@@ -5,14 +5,22 @@ public class Health : MonoBehaviour
     public float maxHealth = 100f;
     public float currentHealth = 100f;
     public Transform healthBar;
+    public bool isBoss = false;
+    public RectTransform bossHealthBar;
+    float originalBarSize;
 
     private void Start()
     {
+        originalBarSize = bossHealthBar.sizeDelta.x;
         currentHealth = maxHealth;
+        bossHealthBar.sizeDelta = new Vector2(originalBarSize * currentHealth / maxHealth, bossHealthBar.sizeDelta.y);
     }
-
     public void TakeDamage(float damage)
     {
+        if(isBoss)
+        {
+            bossHealthBar.sizeDelta = new Vector2(originalBarSize * currentHealth / maxHealth, bossHealthBar.sizeDelta.y);
+        }
         currentHealth -= damage;
 
         if (currentHealth <= 0f)
@@ -23,11 +31,13 @@ public class Health : MonoBehaviour
             Destroy(gameObject);
         }
 
-        
-        float healthPercentage = (float)currentHealth / maxHealth;
+        if(!isBoss)
+        {
+            float healthPercentage = (float)currentHealth / maxHealth;
 
         
-        Vector3 newScale = new Vector3(healthPercentage, healthBar.localScale.y, healthBar.localScale.z);
-        healthBar.localScale = newScale;
+            Vector3 newScale = new Vector3(healthPercentage, healthBar.localScale.y, healthBar.localScale.z);
+            healthBar.localScale = newScale;
+        }
     }
 }
